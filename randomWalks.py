466b8ad5be0f0d44aee37d7931a9fcfa6ee71824
Faisal Ahmed <39379134+FaisalAhmed0@users.noreply.github.com>
2019-02-02 17:52:26 +0200

    Add files via upload
    Code that performs a random walk and plot it

---
+++ randomWalks.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 14:02:32 2017
+
+@author: Faisal
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.random import random as rnd
+
+
+plt.close('all')
+def randomWalk(x):
+    xsteps = np.zeros(x)
+    ysteps = np.zeros(x)
+    xs = (-2*rnd(x,))+1
+    ys = (-2*rnd(x,))+1
+    xsteps = np.cumsum(xs)
+    ysteps = np.cumsum(ys)
+    return xsteps,ysteps    
+num_steps =1000
+x,y = randomWalk(num_steps)
+print (len(x))    
+plt.plot(x,y,'bo')
+plt.axes('equal')
